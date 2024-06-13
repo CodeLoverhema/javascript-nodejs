bd950ff7a0fb5659528232af5ea7f2119a517807
Hema <157248702+CodeLoverhema@users.noreply.github.com>
2024-06-13 18:56:22 +0000

    change

---
+++ 01-basice/01.variabal_const.js
@@ -0,0 +1,13 @@
+const account = 1242335
+let accountEmailId = "hema@goole123"
+var accountPassword = "123452"
+account = "jaipur"
+
+
+
+accountEmailId = "hemaarya0202@.com"
+accountPassword = "1111000"
+accountcity = "Delhi"
+// console.log(accountId);
+
+console.table([ accountEmailId,accountEmail,accountPassword,accountCity])
